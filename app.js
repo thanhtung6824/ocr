2fd999d3d9bc04bb3181b325ab1498e92a872cf2
Tung <tung@Tung.local>
2019-04-18 15:36:58 +0700

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-require('./models');
+// require('./models');
 const expressValidator = require('express-validator');
 
 
