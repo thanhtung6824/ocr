e461047ea5a181b7dba1f4491954dd9d69ca23f3
thanhtung6824@gmail.com <thanhtung6824@gmail.com>
2019-04-19 14:46:43 +0700

    update

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(cookieParser());
 app.use(expressValidator());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', configRoutes.checkApiKey, indexRouter);
+app.use('/', configRoutes.checkApiKey, indexRouter);
 app.use('/views', viewRouter);
 
 // catch 404 and forward to error handler
