147f12b094aaf141cad0c12f332c3bba37ccb894
thanhtung6824@gmail.com <thanhtung6824@gmail.com>
2019-05-06 13:57:17 +0700

    update

--- constants/constants.js
+++ constants/constants.js
@@ -1,6 +1,7 @@
 exports.OCR_UPLOAD_API = 'https://apitenten.smartocr.vn/id/v1/recognition';
 exports.OCR_UPLOAD_API_APP = 'https://cmnd.smartocr.vn/id/v1/recognition';
 exports.OCR_LOCAL_UPLOAD_API = 'http://172.17.0.4/id/v1/recognition';
+exports.OCR_BLX_JP = 'http://dlapi.smartocr.net/dlic/v1/recognition';
 
 exports.INACTIVE_MSG_VN = 'Liên hệ qua https://smartocr.vn để demo.';
 exports.INACTIVE_MSG_JP = 'この機能は近日デモ開始予定です。';
