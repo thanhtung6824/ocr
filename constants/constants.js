1a2e5ca116527619641e8ac5c176ab06a03525fd
thanhtung6824@gmail.com <thanhtung6824@gmail.com>
2019-05-30 09:45:27 +0700

    update

--- constants/constants.js
+++ constants/constants.js
@@ -2,6 +2,8 @@ exports.OCR_UPLOAD_API = 'https://apitenten.smartocr.vn/id/v1/recognition';
 exports.OCR_UPLOAD_API_APP = 'https://cmnd.smartocr.vn/id/v1/recognition';
 exports.OCR_LOCAL_UPLOAD_API = 'http://172.17.0.4/id/v1/recognition';
 exports.OCR_BLX_JP = 'https://dlapi.smartocr.net/dlic/v1/recognition';
+exports.OCR_PASSPORT_VN = 'https://ppapi.smartocr.vn/pp/v1/recognition';
+exports.OCR_PASSPORT_JP = 'https://ppapi.smartocr.net/pp/v1/recognition';
 
 exports.INACTIVE_MSG_VN = 'Liên hệ qua https://smartocr.vn để demo.';
 exports.INACTIVE_MSG_JP = 'この機能は近日デモ開始予定です。';
