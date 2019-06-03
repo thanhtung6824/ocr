9b092982b39c139ef919e3c35694058b2e27459e
thanhtung6824@gmail.com <thanhtung6824@gmail.com>
2019-06-03 08:30:41 +0700

    update

--- constants/constants.js
+++ constants/constants.js
@@ -2,7 +2,7 @@ exports.OCR_UPLOAD_API = 'https://apitenten.smartocr.vn/id/v1/recognition';
 exports.OCR_UPLOAD_API_APP = 'https://cmnd.smartocr.vn/id/v1/recognition';
 exports.OCR_LOCAL_UPLOAD_API = 'http://172.17.0.4/id/v1/recognition';
 exports.OCR_BLX_JP = 'https://dlapi.smartocr.net/dlic/v1/recognition';
-exports.OCR_PASSPORT_VN = 'https://ppapi2.smartocr.vn/pp/v1/recognition';
+exports.OCR_PASSPORT_VN = 'https://ppapi.smartocr.vn/pp/v1/recognition';
 exports.OCR_PASSPORT_JP = 'https://ppapi.smartocr.net/pp/v1/recognition';
 
 exports.INACTIVE_MSG_VN = 'Liên hệ qua https://smartocr.vn để demo.';
